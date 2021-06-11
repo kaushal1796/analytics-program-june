0ea67f23cfaf9e47fef6d003333ef16fb31288c4
Kaushal Rindhe <85434295+kaushal1796@users.noreply.github.com>
2021-06-11 11:58:50 -0700

    prgm-0

---
+++ basic program 1st.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 11:51:21 2021
+
+@author: NEW
+"""
+
+X = [1,2,3,4,5]
+Y = [2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as plt
+plt.plot(X,Y)
